b1406e7f6121d6b92d1d9ca457f545c2ed18daa2
Simran <ss1043422@gmail.com>
2025-02-14 12:59:25 +0530

    LeetCode 1352

---
+++ LeetCode3/ProductOfNumbers.java
@@ -0,0 +1,16 @@
+package LeetCode3;
+
+public class ProductOfNumbers {
+
+    public ProductOfNumbers() {
+        
+    }
+    
+    public void add(int num) {
+        
+    }
+    
+    public int getProduct(int k) {
+        return 0;
+    }
+}
