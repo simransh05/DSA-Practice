219f3e659f4f406592be44b200ca0c50f90286dd
Simran <ss1043422@gmail.com>
2025-02-14 13:05:27 +0530

    LeetCode 1352

--- LeetCode3/ProductOfNumbers.java
+++ LeetCode3/ProductOfNumbers.java
@@ -1,16 +1,29 @@
 package LeetCode3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductOfNumbers {
+	List<Integer> prefix ;
 
     public ProductOfNumbers() {
-        
+        prefix = new ArrayList<>();
+        prefix.add(1);
     }
     
     public void add(int num) {
-        
+        if(num==0) {
+        	prefix.clear();
+        	prefix.add(1);
+        }else {
+        	prefix.add(prefix.get(prefix.size() - 1) * num); 
+        }
     }
     
     public int getProduct(int k) {
-        return 0;
+        int size =prefix.size();
+        if(k>=size) {
+        	return 0;
+        }return prefix.get(size-1)/prefix.get(size-1-k);
     }
 }
